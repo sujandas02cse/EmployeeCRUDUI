39de1290424d95f0a9ad6a9441be46c53d2628c0
sujandas02cse <sujandas02csecuet@gmail.com>
2024-07-14 20:34:51 +0600

    d

--- js/controller.js
+++ js/controller.js
@@ -1,87 +1,88 @@
-app.controller("EmployeeController", function ($scope, EmployeeService) {
-  $scope.employees = [];
-  $scope.newEmployee = {};
-  $scope.isEditing = false;
+app.controller("EmployeeController", function($scope, EmployeeService) {
+    $scope.employees = [];
+    $scope.newEmployee = {};
+    $scope.isEditing = false;
 
-  // load all employees
+    // load all employees
 
-  $scope.loadEmployees = function () {
-    EmployeeService.getAll().then(
-      function (response) {
-        $scope.employees = response.data;
-        console.log(response.data);
-      },
-      function (error) {
-        console.error("Error loading employees", error);
-      }
-    );
-  };
 
-  $scope.deleteEmployee = function (employee) {
-    var confirmDelete = confirm(
-      "Are you sure you want to delte this employee?\n" +
-        "ID:" +
-        employee.Id +
-        "\n" +
-        "Name:" +
-        employee.EmployeeName +
-        "\n" +
-        "Date of Joining:" +
-        employee.DOJ +
-        "\n" +
-        "Department:" +
-        employee.Department +
-        "\n" +
-        "Company:" +
-        employee.Company
-    );
-    if (confirmDelete) {
-      EmployeeService.delete(employee.Id).then(
-        function (response) {
-          $scope.loadEmployees();
-        },
-        function (error) {
-          console.error("error deleting employee:", error);
+    $scope.loadEmployees = function() {
+        EmployeeService.getAll().then(
+            function(response) {
+                $scope.employees = response.data;
+                console.log(response.data);
+            },
+            function(error) {
+                console.error("Error loading employees", error);
+            }
+        );
+    };
+
+    $scope.deleteEmployee = function(employee) {
+        var confirmDelete = confirm(
+            "Are you sure you want to delte this employee?\n" +
+            "ID:" +
+            employee.Id +
+            "\n" +
+            "Name:" +
+            employee.EmployeeName +
+            "\n" +
+            "Date of Joining:" +
+            employee.DOJ +
+            "\n" +
+            "Department:" +
+            employee.Department +
+            "\n" +
+            "Company:" +
+            employee.Company
+        );
+        if (confirmDelete) {
+            EmployeeService.delete(employee.Id).then(
+                function(response) {
+                    $scope.loadEmployees();
+                },
+                function(error) {
+                    console.error("error deleting employee:", error);
+                }
+            );
         }
-      );
-    }
-  };
+    };
 
-  $scope.addOrUpdateEmployee = function () {
-    if ($scope.isEditing) {
-      EmployeeService.update($scope.newEmployee).then(
-        function(response){
-          $scope.loadEmployees();
-          $scope.newEmployee={};
-          $scope.isEditing=false;
-          alert("Employee updated successfully");
-          $('#addEmployeeModal').modal('hide');
-        },
-        function(error){
-          console.error("Error updating employee",error);
+    $scope.addOrUpdateEmployee = function() {
+        if ($scope.isEditing) {
+            EmployeeService.update($scope.newEmployee).then(
+                function(response) {
+                    $scope.loadEmployees();
+                    $scope.newEmployee = {};
+                    $scope.isEditing = false;
+                    alert("Employee updated successfully");
+                    $('#addEmployeeModal').modal('hide');
+                },
+                function(error) {
+                    console.error("Error updating employee", error);
+                }
+            );
+        } else {
+            EmployeeService.create($scope.newEmployee).then(
+                function(response) {
+                    $scope.loadEmployees();
+                    $scope.newEmployee = {};
+                    alert("Employee added successfully");
+                    $("#addEmployeeModal").modal("hide");
+                },
+                function(error) {
+                    console.error("Error adding employee:", error);
+                }
+            );
         }
-      );
-    } else {
-      EmployeeService.create($scope.newEmployee).then(
-        function (response) {
-          $scope.loadEmployees();
-          $scope.newEmployee = {};
-          alert("Employee added successfully");
-          $("#addEmployeeModal").modal("hide");
-        },
-        function (error) {
-          console.error("Error adding employee:", error);
-        }
-      );
-    }
-  };
+    };
 
-  $scope.editEmployee=function(employee){
-    $scope.newEmployee=angular.copy(employee);
-    $scope.newEmployee.DOJ=new Date(employee.DOJ);
-    $scope.isEditing=true;
-    $('#addEmployeeModal').modal('show');
-  };
+    $scope.editEmployee = function(employee) {
+        $scope.newEmployee = angular.copy(employee);
+        $scope.newEmployee.DOJ = new Date(employee.DOJ);
+        $scope.isEditing = true;
+        $('#addEmployeeModal').modal('show');
+    };
 
-  $scope.loadEmployees();
+    $scope.loadEmployees();
 });
